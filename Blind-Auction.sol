22b8f3b08adabeea21e878577a2983a3869cda21
Michael McClimon <mickeymcclimon@gmail.com>
2021-08-14 08:58:49 -0400

    adding in functions

--- Blind-Auction.sol
+++ Blind-Auction.sol
@@ -2,6 +2,38 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract BlindAuction {
 
+  // Variables
+  struct Bid {
+    bytes32 blindedBid;
+    uint deposit;
+  }
+
+
+address payable public beneficiary;
+uint public biddingEnd;
+uint public revealEnd;
+bool public ended;
+
+  // Events
+
+  // Functions
+
+  function genereateBlindedBidBytes32() {
+
+  }
+
+  function bid() {
+
+  }
+
+  function reveal() {
+    
+  }
+
+  function auctionEnd() {
+
+  }
+
   function withdraw() {
   
   }
